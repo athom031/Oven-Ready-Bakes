dfe20236a577a055cd2fd19d20a1aad1386fde71
alexthomas <alexjosthomas@gmail.com>
2023-10-11 18:49:04 -0700

    added simple nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,16 @@
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <div>
-      <h1>
-        NavBar will go here
-      </h1>
+        <nav>
+            <Link to="/home">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>  
+        </nav>
     </div>
   );
 }
