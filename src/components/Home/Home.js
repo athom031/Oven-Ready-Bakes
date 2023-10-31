f80797ecbafb5071e9a1c11ba4a2ac1fa7413933
Alex Thomas <thomaalex@meta.com>
2023-10-30 22:54:09 -0700

    home page skeleton

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,40 @@
+// Home.js
+import React from 'react';
 import './Home.css';
-import InstagramEmbeding from '../Miscellaneous/InstagramEmbeding';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 function Home() {
     return (
-        <div>
-            <div>
-                Title will go here
+        <div className="home-container">
+            <div className="intro-container">
+                <div className="logo-container">
+                    <img src={process.env.PUBLIC_URL + '/assets/logo-transparent.png'} alt="Logo" className="logo" />
+                </div>
+                <div className="intro-text">
+                    <h2>Welcome to [Bakery Name]!</h2>
+                    <p>Short introductory text about the bakery and what makes it special.</p>
+                </div>
             </div>
 
-            <div className='horizontal-row-body'>
-                <div>
-                    Oven Ready Bakes Intro
+            <div className="summary-container">
+                <h3>Main Items</h3>
+                <p>Include descriptions of the main baked goods here.</p>
+            </div>
+
+            <div className="social-media-container">
+                <div className="social-media-text">
+                    <h3>Come Follow My Baking Adventure</h3>
+                    <p>Description of the weekly Instagram posts and the bakery's journey.</p>
                 </div>
-                <div>
-                    <InstagramEmbeding />
+                <div className="instagram-embed">
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <InstagramEmbed url="https://www.instagram.com/reel/CugSmsosGPp/" maxWidth={400} />
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 }
-
 
 export default Home;
