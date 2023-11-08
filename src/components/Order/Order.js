fb095527e5baf4f8907b02cd6df1b3d95c8da249
Jan Pazhayampallil <jpazhaya@gmail.com>
2023-11-07 22:40:23 -0800

    Added google form to Order page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,9 +3,9 @@ import './Order.css';
 function Order() {
   return (
     <div>
-      <h1>
-        Order Section
-      </h1>
+      <div className='form'>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRxAmB0-6OPr8Rr7GAttU7RvOE-_0SHsvXvh_F9YZUvANZEw/viewform?embedded=true" width="640" height="1027" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      </div>
     </div>
   );
 }
