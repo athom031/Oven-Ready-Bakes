b372cfc4f92f968316acfb7fd4048de3f2f08209
Alex Thomas <thomaalex@meta.com>
2023-11-08 10:39:40 -0800

    fixing build error for Order.js

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,11 +4,20 @@ function Order() {
   return (
     <div>
       <div className='form'>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRxAmB0-6OPr8Rr7GAttU7RvOE-_0SHsvXvh_F9YZUvANZEw/viewform?embedded=true" width="640" height="1027" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe
+          title="Order Form"
+          src="https://docs.google.com/forms/d/e/1FAIpQLScRxAmB0-6OPr8Rr7GAttU7RvOE-_0SHsvXvh_F9YZUvANZEw/viewform?embedded=true"
+          width="640"
+          height="1027"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
       </div>
     </div>
   );
 }
-
 
 export default Order;
