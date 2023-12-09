8ab5f23a74ad8b52210529ee317a2e8100b92f18
Alex Thomas <thomaalex@meta.com>
2023-12-08 18:22:26 -0800

    contacts phone section(portrait)

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
 
     const contactBlurb = `We're not just about baking goodies; we're all about creating heartfelt, homemade yumminess that warms your soul. `
     + `At Oven Ready, we're prepared to whip up an array of delectable treats tailored precisely to your preferences. `
-    + `From fruity burst muffins to an assortment of cookies and macarons, each treat is crafted with care and love. `
+    + `From fruity muffins to an assortment of cookies and macarons, each treat is crafted with care and love. `
     + `Browse our selection, place your order, and experience the joy of receiving a package filled with handcrafted delights. `
     + `Our commitment to exceptional customer service ensures a seamless ordering process. If you have any dietary needs, special requests, or questions, please reach out to us via email or Instagram. `
     + `Join us on our baking escapades on Instagram and be a part of our sweet story. `
@@ -58,7 +58,9 @@ function Contact() {
             <div className="contact-blurb-container">
                 <div className="contact-blurb">
                     <div className="text-bubble">
-                        <p>{contactBlurb}</p>
+                        <p>
+                            {contactBlurb}
+                        </p>
                     </div>
                 </div>
                 <div className = "contact-baked-items">
